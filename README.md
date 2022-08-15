8e185d5a263739901d86ab42b0c887a8ef5a99c1
AlexisT37 <alexistroitzky@gmail.com>
2022-08-15 14:57:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# OCP5BlogPHP
+
+The 5th Open Classrooms project: A php Blog
+
+## Requirements
+
+we will use php 4.3.0 as it is the one I use in company setting.
+We will use Laragon to run the project.
+
+we will use pull requests with github for the workflow.
+
+### Requirements for the project
+
+bootstrap premade OpenClassrooms template
+twig
+other
---
+++ src/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My BlogPost P5</title>
+</head>
+
+<body>
+    <h1>Alexis Troïtzky</h1>
+    <h2>Je fabrique des programmes et des sites avec mes mains pleines de doigts</h2>
+    <img src="../assets/images/alex.png" width="200" height="256" alt="photo-alex">
+
+    <form action="index.php" method="post">
+        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="subject" placeholder="Subject">
+        <textarea name="message" id="" cols="10" rows="5" placeholder="Message"></textarea>
+        <button type="submit">Send</button>
+    </form>
+    <div class="social">
+        <a href="https://www.linkedin.com/in/alexis-tro%C3%AFtzky-245b66211/">
+            <img src="../assets/images/Linkedin.png" width="64" height="64" alt="linkedin">
+        </a>
+        <a href="https://www.twitter.com/">
+            <img src="../assets/images/reddit.png" width="64" height="64" alt="reddit">
+        </a>
+    </div>
+    <footer>
+        <a href="#">Administration</a>
+        <p>&copy; 2022</p>
+    </footer>
+
+    <body>
+
+</html>
