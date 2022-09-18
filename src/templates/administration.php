7dc4069a3a8dbc781f0d365248e9232d7b7f2904
AlexisT37 <alexistroitzky@gmail.com>
2022-09-18 17:49:09 +0200

    feat: add back button and user in admin

--- src/templates/administration.php
+++ src/templates/administration.php
@@ -1,30 +1,51 @@
 <html>
 
+<?php 
+// Set admin flag
+if (!empty($_COOKIE['TOKEN'])) {
+    $myToken = $_COOKIE['TOKEN'];
+    $adminJWTCheck = new JWT();
+    $adminInJWT = $adminJWTCheck->isAdmin($myToken);
+    if ($adminInJWT === true) {
+        $admin = 1;
+    } else {
+        $admin = 0;
+    }
+} else {
+    $admin = 0;
+}
+
+
+// Set User flag
+if (!empty($_COOKIE['TOKEN'])) {
+    $currentUserPayload = $adminJWTCheck->getPayload($myToken);
+    $currentUser = $currentUserPayload['username'];
+}
+?>
 <head>
 
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+<title>Le blog d'Alexis - Apprenez le PHP</title>
 
-    <title>Administration</title>
+<!-- Bootstrap Core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- Theme CSS -->
+<link href="css/freelancer.min.css" rel="stylesheet">
 
-    <!-- Theme CSS -->
-    <link href="css/freelancer.min.css" rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
-    <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
@@ -32,7 +53,27 @@
 </head>
 
 <body class="index">
+<br>
+<br>
+<!-- Navigation -->
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php#page-top">Alexis Troïtzky</a>
+            </div>
 
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div>
+                <h4 class="skills currentUser" >Bonjour, <?php echo $currentUser?></h4>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
 
     <!-- Portfolio Grid Section -->
     <section id="portfolio">
