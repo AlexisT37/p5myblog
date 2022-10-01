e26e4fff0e8426c5df3a69a5825a51a4bc4317b5
AlexisT37 <alexistroitzky@gmail.com>
2022-10-01 11:36:18 +0200

    feat: export layout logic

---
+++ src/controllers/layout.php
@@ -0,0 +1,45 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['actionLogout'])) {
+    func();
+}
+function func()
+{
+    unset($_COOKIE['TOKEN']);
+}
+
+if (isset($_GET['logout'])) {
+
+    setcookie("TOKEN", null, 1);
+
+    setcookie($_COOKIE['TOKEN'], '', time() - 3600);
+
+    unset($_COOKIE['TOKEN']);
+
+    session_destroy();
+
+    header('Location: index.php');
+
+    exit;
+}
+
+// Set admin flag
+if (!empty($_COOKIE['TOKEN'])) {
+    $myToken = $_COOKIE['TOKEN'];
+    $adminJWTCheck = new JWT();
+    $adminInJWT = $adminJWTCheck->isAdmin($myToken);
+    if ($adminInJWT === true) {
+        $admin = 1;
+    } else {
+        $admin = 0;
+    }
+} else {
+    $admin = 0;
+}
+
+
+// Set User flag
+if (!empty($_COOKIE['TOKEN'])) {
+    $currentUserPayload = $adminJWTCheck->getPayload($myToken);
+    $currentUser = $currentUserPayload['username'];
+}
+?>
--- src/templates/layout.php
+++ src/templates/layout.php
@@ -1,51 +1,10 @@
+
+
+
+
 <?php
-if ($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['actionLogout'])) {
-    func();
-}
-function func()
-{
-    unset($_COOKIE['TOKEN']);
-}
-
-if (isset($_GET['logout'])) {
-
-    setcookie("TOKEN", null, 1);
-
-    setcookie($_COOKIE['TOKEN'], '', time() - 3600);
-
-    unset($_COOKIE['TOKEN']);
-
-    session_destroy();
-
-    header('Location: index.php');
-
-    exit;
-}
-
-// Set admin flag
-if (!empty($_COOKIE['TOKEN'])) {
-    $myToken = $_COOKIE['TOKEN'];
-    $adminJWTCheck = new JWT();
-    $adminInJWT = $adminJWTCheck->isAdmin($myToken);
-    if ($adminInJWT === true) {
-        $admin = 1;
-    } else {
-        $admin = 0;
-    }
-} else {
-    $admin = 0;
-}
-
-
-// Set User flag
-if (!empty($_COOKIE['TOKEN'])) {
-    $currentUserPayload = $adminJWTCheck->getPayload($myToken);
-    $currentUser = $currentUserPayload['username'];
-}
+require('../src/controllers/layout.php') ;
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
