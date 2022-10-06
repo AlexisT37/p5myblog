4209fc9a2c0b82b0ddef04b6c93ea299f04bb937
AlexisT37 <alexistroitzky@gmail.com>
2022-10-06 16:29:18 +0200

    remove commented code, throw error

--- src/config.php
+++ src/config.php
@@ -1,17 +1,21 @@
 <?php
 
-// session_start();
-
 const SECRET = 'p9uh020dp03bpo0ugouontu';
-
 
 $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = "root"; /* Password */
 $dbname = "oc4"; /* Database name */
 
-$con = mysqli_connect($host, $user, $password, $dbname);
-// Check connection
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+try {
+
+    $con = mysqli_connect($host, $user, $password, $dbname);
+
+    // Check connection
+    if (!$con) {
+        // die("Connection failed: " . mysqli_connect_error());
+        throw new Exception("Connection failed.");
+    }
+} catch (Exception $e) {
+    $errorMessage = $e->getMessage();
 }
