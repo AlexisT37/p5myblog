4c61e2bf22931b485741446f40465f49f07324a8
AlexisT37 <alexistroitzky@gmail.com>
2022-10-21 22:10:58 +0200

    feat: add lib database variables to .env

--- src/lib/database.php
+++ src/lib/database.php
@@ -2,14 +2,24 @@
 
 namespace Application\Lib\Database;
 
+require_once('../src/classes/dotenv.php');
+use Dotenv\DotEnv;
+
+
 class DatabaseConnection
 {
     public ?\PDO $database = null;
 
     public function getConnection(): \PDO
     {
+
+        (new DotEnv("../.env"))->load();
+        $host = getenv('HOST');
+        $user = getenv('USER');
+        $password = getenv('PASSWORD');
+        $dbname = getenv('DBNAME');
         if ($this->database === null) {
-            $this->database = new \PDO('mysql:host=localhost;dbname=oc4;charset=utf8', 'root', 'root');
+            $this->database = new \PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
         }
 
         return $this->database;
